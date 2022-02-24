99cfaa133c246958b5e4421a951c696fa16d3e6e
Solenov <colenov@yandex.ru>
2022-02-24 23:55:48 +0300

    Add script gendiff.py

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+    print(args.first_file, args.second_file)
+
+
+if __name__ == '__main__':
+    main()
