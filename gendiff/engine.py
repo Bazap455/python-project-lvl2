98dfac8f515a1a61ff763932f141a92bf8b80ba6
Solenov <colenov@yandex.ru>
2022-03-13 21:45:20 +0300

    Add engine.py with generate_diff

---
+++ gendiff/engine.py
@@ -0,0 +1,36 @@
+import json
+
+
+def run(args):
+    print(generate_diff(args.first_file, args.second_file, format='string'))
+
+
+def generate_diff(first_file, second_file, format=''):
+    diff = []
+
+    data_before = json.load(open(first_file))
+    data_after =  json.load(open(second_file))
+
+    keys_before = data_before.keys()
+    keys_after = data_after.keys()
+    all_keys = list(set(list(keys_before) + list(keys_after)))
+    all_keys.sort()
+
+    added_keys = keys_after - keys_before
+    deleted_keys = keys_before - keys_after
+    common_keys = keys_before & keys_after
+
+    for key in all_keys:
+        template = '  {symbol} {key}: {value}'
+    
+        if key in added_keys:
+            diff.append(template.format(symbol='+', key=key, value=data_after[key]))
+        elif key in deleted_keys:
+            diff.append(template.format(symbol='-', key=key, value=data_before[key]))
+        elif key in common_keys and (data_before[key] == data_after[key]):
+            diff.append(template.format(symbol=' ', key=key, value=data_before[key]))
+        else:
+            diff.append(template.format(symbol='-', key=key, value=data_before[key]))
+            diff.append(template.format(symbol='+', key=key, value=data_after[key]))
+
+    return '{\n' + '\n'.join(diff) + '\n}'
