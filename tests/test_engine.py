99beafae56b1582ebd15705cbc9a256b7b2b6d78
Solenov <colenov@yandex.ru>
2022-03-27 21:38:20 +0300

    Add test for yaml format

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -3,14 +3,22 @@ from gendiff.engine import generate_diff
 
 
 @pytest.fixture
-def expected_json():
-    f = open('tests/fixtures/expected_json.txt')
+def expected_result():
+    f = open('tests/fixtures/expected_result.txt')
     return f.read()
 
 
-def test_generate_diff(expected_json):
+def test_generate_diff_json(expected_result):
     diff = generate_diff(
         'tests/fixtures/file1.json',
         'tests/fixtures/file2.json',
     )
-    assert diff == expected_json
+    assert diff == expected_result
+
+
+def test_generate_diff_yaml(expected_result):
+    diff = generate_diff(
+        'tests/fixtures/file1.yaml',
+        'tests/fixtures/file2.yaml',
+    )
+    assert diff == expected_result
